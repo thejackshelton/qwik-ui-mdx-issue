1e2a3b18b34eeb5942a8a0b9c5bd139381c432f9
Jack Shelton <me@jackshelton.com>
2023-07-03 12:23:12 -0500

    added font

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,6 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
+import "@fontsource/poppins/400.css";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
